7d35f57443742f9fb22d1eaeca68da4fca15e294
kxnst <ip311s03>
2024-04-20 15:35:29 +0700

    Fix regex and constructor, #5

--- src/docparserlib/c_code_parser.h
+++ src/docparserlib/c_code_parser.h
@@ -14,23 +14,23 @@ namespace dp {
         std::string _cPath = "";
         std::vector<dp::DocUnit> _docData;
 
-        const std::regex _commentPattern = regex(R"(\/\/\/(.*))");
-        const std::regex _briefPattern = regex(R"(@brief\s+(.+))");
-        const std::regex _paramPattern = regex(R"(@param\s+(\w+)\s+(.*))");
-        const std::regex _returnPattern = regex(R"(@return\s+(.+))");
-        const std::regex _notePattern = regex(R"(@note\s+(.+))");
-        const std::regex _throwPattern = regex(R"(@throw\s+(.+))");
-        const std::regex _functionPattern
-                = regex(R"((const\s+)?(unsigned\s+|signed\s+)?(long\s+)?(long\s+)?(\w+)\s+(\w+)\s*\((.*?)\)\s*[\;\{])");
+        const std::regex _commentPattern = std::regex(R"(\/\/\/(.*))");
+        const std::regex _briefPattern = std::regex(R"(@brief\s+(.+))");
+        const std::regex _paramPattern = std::regex(R"(@param\s+(\w+)\s+(.*))");
+        const std::regex _returnPattern = std::regex(R"(@return\s+(.+))");
+        const std::regex _notePattern = std::regex(R"(@note\s+(.+))");
+        const std::regex _throwPattern = std::regex(R"(@throw\s+(.+))");
+        const std::regex _functionPattern = std::regex(
+                R"((const\s+)?(unsigned\s+|signed\s+)?(long\s+)?(long\s+)?(\w+)\s+(\w+)\s*\((.*?)\)\s*[\;\{])");
 
     public:
         CCodeParser(std::string);
-        CCodeParser(char*);
+        CCodeParser(const char*);
         CCodeParser();
 
         std::string GetPath();
         void SetPath(std::string);
-        void SetPath(char*);
+        void SetPath(const char*);
         std::vector<DocUnit> GetDocs();
 
         std::vector<DocUnit> Parse();
