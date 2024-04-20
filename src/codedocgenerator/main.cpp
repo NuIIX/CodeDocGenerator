43445e2f3f58c4110a7f4a2e3aafc205d6a49bd1
kxnst <ip311s03>
2024-04-20 15:38:40 +0700

    Add example of how docparserlib works, #5

--- src/codedocgenerator/main.cpp
+++ src/codedocgenerator/main.cpp
@@ -1,7 +1,17 @@
+#include <docparserlib/c_code_parser.h>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello, World!\n";
+    dp::CCodeParser parser;
+
+    try {
+        parser.SetPath("example/docexample.c");
+        parser.Parse();
+        parser.PrintDocs();
+    } catch (std::invalid_argument iaex) {
+        std::cerr << iaex.what() << std::endl;
+    }
+
     return 0;
 }
