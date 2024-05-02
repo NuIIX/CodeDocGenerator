1648be58ed1a6f7306bf79fda6abb64add6af2d4
kxnst <ip311s03>
2024-05-02 23:53:04 +0700

    Add example of work to main, #9

--- src/codedocgenerator/main.cpp
+++ src/codedocgenerator/main.cpp
@@ -1,15 +1,19 @@
 #include <docparserlib/c_code_parser.h>
+#include <htmlgeneratorlib/template_processor.h>
 #include <iostream>
 
 int main()
 {
     dp::CCodeParser parser;
+    hg::TemplateProcessor templater;
+    std::vector<dp::DocUnit> docData;
 
     try {
         parser.SetPath("example/docexample.c");
-        parser.Parse();
-        parser.PrintDocs();
-    } catch (const std::invalid_argument& iaex) {
+        docData = parser.Parse();
+        templater.SetDocs(docData);
+        templater.CreateHtml("output.html");
+    } catch (std::invalid_argument iaex) {
         std::cerr << iaex.what() << std::endl;
     }
 
