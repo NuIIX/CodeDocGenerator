62fc971b46d6b83bf6209de23ad9aee13d01b171
kxnst <ip311s03>
2024-05-04 00:27:31 +0700

    Add argument parser, #16

---
+++ src/codedocgenerator/argument_parser.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#ifndef ARGUMENT_PARSER_H
+#define ARGUMENT_PARSER_H
+
+#include <string>
+#include <vector>
+
+namespace cdg {
+    class ArgumentParser {
+    private:
+        std::vector<std::string> _args;
+        std::string _outPath;
+        std::string _inPath;
+        std::string _name;
+        bool _saveFile = false;
+
+        void PrintHelp();
+        void CheckArgsNext(const size_t&, const std::string&);
+
+    public:
+        ArgumentParser(const std::vector<std::string>&);
+        ArgumentParser();
+
+        const std::string& GetOutPath() const;
+        const std::string& GetInPath() const;
+        const std::string& GetName() const;
+        std::string GetFileName();
+        const bool& GetSaveFileState() const;
+
+        void Parse();
+    };
+}
+
+#endif // !ARGUMENT_PARSER_H
