57044eea61fba4b82d14a9a9e2ab908d846a5d57
kxnst <ip311s03>
2024-05-04 00:27:52 +0700

    Add CLI, #16

--- src/codedocgenerator/main.cpp
+++ src/codedocgenerator/main.cpp
@@ -1,23 +1,55 @@
+#include "argument_parser.h"
 #include <docparserlib/c_code_parser.h>
 #include <fstream>
 #include <htmlgeneratorlib/file_utils.h>
 #include <htmlgeneratorlib/template_processor.h>
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
-    dp::CCodeParser parser;
-    hg::TemplateProcessor templater;
-    std::vector<dp::DocUnit> docData;
-
     try {
-        parser.SetPath("example/docexample.cpp");
-        parser.Parse();
-        docData = parser.GetDocs();
-        templater.SetDocs(docData);
-        templater.CreateHtml("output.html");
+        cdg::ArgumentParser argsParser(std::vector<std::string>(argv + 1, argv + argc));
+        argsParser.Parse();
+
+        if (argsParser.GetInPath().empty()) {
+            throw std::invalid_argument("-c does not contain a path!");
+        }
+
+        std::string outPath;
+
+        if (!argsParser.GetOutPath().empty()) {
+            outPath = argsParser.GetOutPath() + "_docs";
+        } else if (!argsParser.GetName().empty()) {
+            outPath = argsParser.GetName() + "_docs";
+        } else {
+            outPath = argsParser.GetFileName() + "_docs";
+        }
+
+        dp::CCodeParser codeParser(argsParser.GetInPath());
+        codeParser.Parse();
+
+        if (argsParser.GetSaveFileState()) {
+            std::ofstream outTextFile = hg::OpenFileWrite(outPath + ".txt");
+            codeParser.PrintDocs(outTextFile);
+            outTextFile.close();
+        }
+
+        hg::TemplateProcessor templateProcessor(codeParser.GetDocs());
+
+        if (!argsParser.GetName().empty()) {
+            templateProcessor.SetHead(argsParser.GetName());
+        }
+
+        templateProcessor.CreateHtml(outPath + ".html");
+    } catch (const std::out_of_range& oorex) {
+        std::cerr << "Error: " << oorex.what();
+        return 1;
+    } catch (const std::runtime_error& reex) {
+        std::cerr << "Error: " << reex.what();
+        return 1;
     } catch (const std::invalid_argument& iaex) {
-        std::cerr << iaex.what() << std::endl;
+        std::cerr << "Error: " << iaex.what();
+        return 1;
     }
 
     return 0;
