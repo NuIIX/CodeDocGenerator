08dee5c1ede19ca2684ca5674816c8521bca2119
kxnst <ip311s03>
2024-05-06 19:24:35 +0700

    Update tests, #25

--- test/docparserlib_test.cpp
+++ test/docparserlib_test.cpp
@@ -5,100 +5,78 @@
 
 CTEST(docparserlib_test, get_set_path_test)
 {
-    dp::CCodeParser cCodeParser;
-    std::string path = "D:/Git";
+    dp::CCodeParser parser;
+    std::string pathExample = "D:/Git";
 
-    cCodeParser.SetPath(path);
-    std::string resultPath = cCodeParser.GetPath();
+    parser.SetPath(pathExample);
+    std::string actualPath = parser.GetPath();
 
-    ASSERT_STR(path.c_str(), resultPath.c_str());
+    ASSERT_STR(pathExample.c_str(), actualPath.c_str());
 }
 
-CTEST(docparserlib_test, designer_test)
+CTEST(docparserlib_test, constructor_test)
 {
-    std::string path = "D:/Git";
-    std::string resultPath = "";
-    dp::CCodeParser cCodeParser = dp::CCodeParser();
-    dp::CCodeParser cCodeParserStringPath = dp::CCodeParser(path);
-    dp::CCodeParser cCodeParserCharPath = dp::CCodeParser(path.c_str());
+    std::string pathExample = "D:/Git";
 
-    resultPath = cCodeParser.GetPath();
-    ASSERT_STR("", resultPath.c_str());
+    dp::CCodeParser parser;
+    ASSERT_STR("", parser.GetPath().c_str());
 
-    resultPath = cCodeParserStringPath.GetPath();
-    ASSERT_STR(path.c_str(), resultPath.c_str());
+    dp::CCodeParser parserStringPath(pathExample);
+    ASSERT_STR(pathExample.c_str(), parserStringPath.GetPath().c_str());
 
-    resultPath = cCodeParserCharPath.GetPath();
-    ASSERT_STR(path.c_str(), resultPath.c_str());
+    dp::CCodeParser parserCharPath(pathExample.c_str());
+    ASSERT_STR(pathExample.c_str(), parserCharPath.GetPath().c_str());
 }
 
 CTEST(docparserlib_test, pars_method_test)
 {
-    std::string path = "example/docexample.cpp";
-    dp::DocUnit exampleDocUnit
-            = {"Add",
-               "Sum a and b",
-               {"notemonnte", "note2"},
-               {{"a", "First"}, {"b", "Second"}, {"c", "Third"}},
-               {"std::invalid_argument If one of args not a number"},
-               {true, "signed long long int", "sum2", "int a, int b, int c"}};
+    std::string actualPath = "example/docexample.cpp";
+    dp::DocFunction expectedFunction(true, "signed long long int", "sum2", "int a, int b, int c");
+    dp::DocUnit expectedDocUnit(
+            "Add",
+            "Sum a and b",
+            {"notemonnte", "note2"},
+            {{"a", "First"}, {"b", "Second"}, {"c", "Third"}},
+            {"std::invalid_argument If one of args not a number"},
+            expectedFunction);
 
-    dp::CCodeParser cCodeParser = dp::CCodeParser(path);
-    cCodeParser.Parse();
-    dp::DocUnit resultDocUnit = cCodeParser.GetDocs().at(1);
+    dp::CCodeParser parser(actualPath);
+    parser.Parse();
+    dp::DocUnit actualDocUnit = parser.GetDocs().at(1);
 
-    ASSERT_TRUE(exampleDocUnit == resultDocUnit);
+    ASSERT_TRUE(expectedDocUnit == actualDocUnit);
 }
 
 CTEST(docparserlib_test, regex_comment_test)
 {
-    dp::CCodeParser cCodeParser;
-    std::regex commentPattern = cCodeParser.GetCommentPattern();
+    dp::CCodeParser parser;
+    std::regex commentPattern = parser.GetCommentPattern();
 
-    std::string exampleReg = "///@throw std::invalid_argument If one of args not a number";
-    ASSERT_TRUE(std::regex_search(exampleReg, commentPattern));
-
-    exampleReg = "//@throw std::invalid_argument If one of args not a number";
-    ASSERT_FALSE(std::regex_search(exampleReg, commentPattern));
-
-    exampleReg = "//_/@throw std::invalid_argument If one of args not a number";
-    ASSERT_FALSE(std::regex_search(exampleReg, commentPattern));
+    ASSERT_TRUE(std::regex_search("///@throw std::invalid_argument If one of args not a number", commentPattern));
+    ASSERT_FALSE(std::regex_search("//@throw std::invalid_argument If one of args not a number", commentPattern));
+    ASSERT_FALSE(std::regex_search("//_/@throw std::invalid_argument If one of args not a number", commentPattern));
 }
 
 CTEST(docparserlib_test, regex_brief_test)
 {
-    dp::CCodeParser cCodeParser;
-    std::regex briefPattern = cCodeParser.GetBriefPattern();
+    dp::CCodeParser parser;
+    std::regex briefPattern = parser.GetBriefPattern();
 
-    std::string exampleReg = "///@brief Add";
-    ASSERT_TRUE(std::regex_search(exampleReg, briefPattern));
-
-    exampleReg = "//@brief Add";
-    ASSERT_TRUE(std::regex_search(exampleReg, briefPattern));
-
-    exampleReg = "//_/@brief Add";
-    ASSERT_TRUE(std::regex_search(exampleReg, briefPattern));
-
-    exampleReg = "/@bridef Add";
-    ASSERT_FALSE(std::regex_search(exampleReg, briefPattern));
+    ASSERT_TRUE(std::regex_search("///@brief Add", briefPattern));
+    ASSERT_TRUE(std::regex_search("//@brief Add", briefPattern));
+    ASSERT_TRUE(std::regex_search("//_/@brief Add", briefPattern));
+    ASSERT_FALSE(std::regex_search("/@bridef Add", briefPattern));
 }
 
 CTEST(docparserlib_test, regex_param_test)
 {
-    dp::CCodeParser cCodeParser;
-    std::regex paramPattern = cCodeParser.GetParamPattern();
+    dp::CCodeParser parser;
+    std::regex paramPattern = parser.GetParamPattern();
 
-    std::string exampleReg = "///@param a First";
-    ASSERT_TRUE(std::regex_search(exampleReg, paramPattern));
-
-    exampleReg = "//@param a First";
-    ASSERT_TRUE(std::regex_search(exampleReg, paramPattern));
-
-    exampleReg = "//_/@param a First";
-    ASSERT_TRUE(std::regex_search(exampleReg, paramPattern));
-
-    exampleReg = "/@paradsm a First";
-    ASSERT_FALSE(std::regex_search(exampleReg, paramPattern));
+    ASSERT_TRUE(std::regex_search("///@param a First", paramPattern));
+    ASSERT_TRUE(std::regex_search("//@param a First", paramPattern));
+    ASSERT_TRUE(std::regex_search("//_/@param a First", paramPattern));
+    ASSERT_FALSE(std::regex_search("/@paradsm a First", paramPattern));
 }
 
 CTEST(docparserlib_test, regex_return_test)
@@ -106,69 +84,42 @@ CTEST(docparserlib_test, regex_return_test)
     dp::CCodeParser cCodeParser;
     std::regex returnPattern = cCodeParser.GetReturnPattern();
 
-    std::string exampleReg = "///@return Sum a and b";
-    ASSERT_TRUE(std::regex_search(exampleReg, returnPattern));
-
-    exampleReg = "//@return Sum a and b";
-    ASSERT_TRUE(std::regex_search(exampleReg, returnPattern));
-
-    exampleReg = "//_/@return Sum a and b";
-    ASSERT_TRUE(std::regex_search(exampleReg, returnPattern));
-
-    exampleReg = "/@returnds Sum a and b";
-    ASSERT_FALSE(std::regex_search(exampleReg, returnPattern));
+    ASSERT_TRUE(std::regex_search("///@return Sum a and b", returnPattern));
+    ASSERT_TRUE(std::regex_search("//@return Sum a and b", returnPattern));
+    ASSERT_TRUE(std::regex_search("//_/@return Sum a and b", returnPattern));
+    ASSERT_FALSE(std::regex_search("/@returnds Sum a and b", returnPattern));
 }
 
 CTEST(docparserlib_test, regex_note_test)
 {
-    dp::CCodeParser cCodeParser;
-    std::regex notePattern = cCodeParser.GetNotePattern();
+    dp::CCodeParser parser;
+    std::regex notePattern = parser.GetNotePattern();
 
-    std::string exampleReg = "///@note notemonnte";
-    ASSERT_TRUE(std::regex_search(exampleReg, notePattern));
-
-    exampleReg = "//@note notemonnte";
-    ASSERT_TRUE(std::regex_search(exampleReg, notePattern));
-
-    exampleReg = "//_/@note notemonnte";
-    ASSERT_TRUE(std::regex_search(exampleReg, notePattern));
-
-    exampleReg = "/@nosdte notemonsdndsdfte";
-    ASSERT_FALSE(std::regex_search(exampleReg, notePattern));
+    ASSERT_TRUE(std::regex_search("///@note notemonnte", notePattern));
+    ASSERT_TRUE(std::regex_search("//@note notemonnte", notePattern));
+    ASSERT_TRUE(std::regex_search("//_/@note notemonnte", notePattern));
+    ASSERT_FALSE(std::regex_search("/@nosdte notemonsdndsdfte", notePattern));
 }
 
 CTEST(docparserlib_test, regex_throw_test)
 {
-    dp::CCodeParser cCodeParser;
-    std::regex throwPattern = cCodeParser.GetThrowPattern();
+    dp::CCodeParser parser;
+    std::regex throwPattern = parser.GetThrowPattern();
 
-    std::string exampleReg = "///@throw std::invalid_argument If one of args not a number";
-    ASSERT_TRUE(std::regex_search(exampleReg, throwPattern));
-
-    exampleReg = "//@throw std::invalid_argument If one of args not a number";
-    ASSERT_TRUE(std::regex_search(exampleReg, throwPattern));
-
-    exampleReg = "//_/@throw std::invalid_argument If one of args not a number";
-    ASSERT_TRUE(std::regex_search(exampleReg, throwPattern));
-
-    exampleReg = "/@thrsdow std::invalid_argumensdfsdsdt If one of args not a number";
-    ASSERT_FALSE(std::regex_search(exampleReg, throwPattern));
+    ASSERT_TRUE(std::regex_search("///@throw std::invalid_argument If one of args not a number", throwPattern));
+    ASSERT_TRUE(std::regex_search("//@throw std::invalid_argument If one of args not a number", throwPattern));
+    ASSERT_TRUE(std::regex_search("//_/@throw std::invalid_argument If one of args not a number", throwPattern));
+    ASSERT_FALSE(std::regex_search("/@thrsdow std::invalid_argumensdfsdsdt If one of args not a number", throwPattern));
 }
 
 CTEST(docparserlib_test, regex_function_test)
 {
-    dp::CCodeParser cCodeParser;
-    std::regex functionPattern = cCodeParser.GetFunctionPattern();
+    dp::CCodeParser parser;
+    std::regex functionPattern = parser.GetFunctionPattern();
 
-    std::string exampleReg = "const signed long long int sum(int a, int b, int c);";
-    ASSERT_TRUE(std::regex_search(exampleReg, functionPattern));
-
-    exampleReg = "const signed long long int sum(int a, int b, int c;";
-    ASSERT_FALSE(std::regex_search(exampleReg, functionPattern));
-
-    exampleReg = "const signed long long int sum int a, int b, int c);";
-    ASSERT_FALSE(std::regex_search(exampleReg, functionPattern));
-
-    exampleReg = "const signed long long |(int sum int a, int b, int c);";
-    ASSERT_FALSE(std::regex_search(exampleReg, functionPattern));
+    ASSERT_TRUE(std::regex_search("const signed long long int sum(int a, int b, int c);", functionPattern));
+    ASSERT_FALSE(std::regex_search("const signed long long int sum(int a, int b, int c;", functionPattern));
+    ASSERT_FALSE(std::regex_search("const signed long long int sum int a, int b, int c);", functionPattern));
+    ASSERT_TRUE(std::regex_search("dp::DocFunction::DocFunction(bool isConst);", functionPattern));
+    ASSERT_TRUE(std::regex_search("vector<string> TemplateProcessor::ConvertDocParamsToStrings()", functionPattern));
 }
